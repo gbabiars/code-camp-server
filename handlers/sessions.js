3eec0d2ab76fdb1a76f08223facae245045d4d6f
Greg Babiars <gbabiars@gmail.com>
2014-03-31 09:09:10 -0700

    cleaned up session by id

--- handlers/sessions.js
+++ handlers/sessions.js
@@ -20,7 +20,8 @@ module.exports = {
         var db = req.server.app.db;
         db.collection('sessions')
             .findOne({ _id: new ObjectID(req.params.id) }, function(err, session) {
-                session.id = s._id;
+                session.id = session._id;
+                delete session._id;
                 reply({ session: session });
             });
     }
