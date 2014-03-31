8dd4af58d3b6543e69b580fe1d44c664f06e7be0
Greg Babiars <gbabiars@gmail.com>
2014-03-31 13:43:38 -0700

    Parsing details to get time and room

--- handlers/scrape.js
+++ handlers/scrape.js
@@ -1,71 +1,103 @@
 var request = require('request-promise'),
     cheerio = require('cheerio'),
     _ = require('lodash'),
-    sessionsUrl = 'http://apr2014.desertcodecamp.com/sessions/all';
+    Q = require('q'),
+    sessionsUrl = 'http://apr2014.desertcodecamp.com/sessions/all',
+    sessionDetailUrl = 'http://apr2014.desertcodecamp.com/session/';
+
+function parseSessionsPage() {
+    return request(sessionsUrl).then(function(data) {
+        var $ = cheerio.load(data),
+            $sessions = $('#sessions');
+
+        // parse html to get data
+        return $sessions.children('h3')
+            .map(function(index, element) {
+                var $el = $(element);
+
+                var sessions = $el.next().find('.sessionList tr#SessionRow')
+                    .map(function(index, element) {
+                        var $el = $(element);
+                        return {
+                            status: $el.attr('class'),
+                            title: $el.find('td:nth-child(3)').text(),
+                            description: $el.find('td:nth-child(4) a').attr('data-content'),
+                            sessionId: $el.find('#hlSessionInfo').attr('href').replace('/session.aspx?SessionId=', '')
+                        }
+                    })
+                    .toArray();
+
+                return {
+                    name: $el.text(),
+                    description: $el.next().find('small').text(),
+                    sessions: sessions
+                }
+            })
+            .toArray();
+    });
+};
+
+function parseSessionDetailPage(session) {
+    console.log('Parsing ' + session.title);
+    return request(sessionDetailUrl + session.sessionId).then(function(data) {
+        var $ = cheerio.load(data);
+
+        session.room = $('#lblRoom').text();
+        session.time = $('#lblTime').text();
+
+        return session;
+    });
+};
 
 function sessions(req, reply) {
 
     var db = req.server.app.db;
 
-    request(sessionsUrl)
-        .then(function(data) {
-            var $ = cheerio.load(data);
-            var $sessions = $('#sessions');
-
-            // parse html to get data
-            var categories = $sessions.children('h3')
-                .map(function(index, element) {
-                    var $el = $(element);
-
-                    var sessions = $el.next().find('.sessionList tr#SessionRow')
-                        .map(function(index, element) {
-                            var $el = $(element);
-                            return {
-                                status: $el.attr('class'),
-                                title: $el.find('td:nth-child(3)').text(),
-                                description: $el.find('td:nth-child(4) a').attr('data-content')
-                            }
-                        })
-                        .toArray();
-
-                    return {
-                        name: $el.text(),
-                        description: $el.next().find('small').text(),
-                        sessions: sessions
-                    }
-                })
-                .toArray();
+    parseSessionsPage().then(function(categories) {
 
 
-            // store off the category data
-            var tracksCollection = db.collection('tracks'),
-                sessionsCollection = db.collection('sessions');
+        // store off the category data
+        var tracksCollection = db.collection('tracks'),
+            sessionsCollection = db.collection('sessions');
 
-            tracksCollection.remove({}, { w: 1 }, function(err, count) {
+        tracksCollection.remove({}, { w: 1 }, function(err, count) {
 
-                var mappedCategories = categories.map(function(c) {
-                    return { name: c.name, description: c.description };
+            var mappedCategories = categories.map(function(c) {
+                return { name: c.name, description: c.description };
+            });
+            tracksCollection.insert(mappedCategories, { w: 1 }, function(err, insertedCategories) {
+
+                insertedCategories.forEach(function(c, i) {
+                    categories[i].sessions.forEach(function(s) {
+                        s.categoryId = c._id;
+                    });
                 });
-                tracksCollection.insert(mappedCategories, { w: 1 }, function(err, insertedCategories) {
 
-                    insertedCategories.forEach(function(c, i) {
-                        categories[i].sessions.forEach(function(s) {
-                            s.categoryId = c._id;
-                        });
+                sessionsCollection.remove({}, { w: 1 }, function(err, count) {
+
+                    var flattenedSessions = _.flatten(categories, 'sessions'),
+                        sessionDetailsPromises = [];
+
+                    flattenedSessions.forEach(function(s) {
+                        var sessionPromise = parseSessionDetailPage(s);
+                        sessionDetailsPromises.push(sessionPromise);
                     });
 
-                    sessionsCollection.remove({}, { w: 1 }, function(err, count) {
+                    Q.all(sessionDetailsPromises).done(function(data) {
+                        console.log('Finished loading all sessions');
 
-                        sessionsCollection.insert(_.flatten(categories, 'sessions'), { w: 1 }, function(err, insertedSessions) {
+                        sessionsCollection.insert(flattenedSessions, { w: 1 }, function(err, insertedSessions) {
                             reply(insertedSessions.length);
-                        })
+                        });
 
                     });
 
                 });
 
             });
+
         });
+    });
 
 };
 
